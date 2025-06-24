b3bcc49a60405e10d07fce73d335f564a15d0a40
Rafael Andrade <rafael.io@outlook.com>
2025-06-23 22:38:14 -0300

    Implementa layout simples

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,59 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
-import { MoviesMockService } from './shared/mock/movies.mock.service';
+import { LayoutComponent } from '@core/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`
+  imports: [RouterOutlet, LayoutComponent],
+  template: `<app-layout>
+              <router-outlet></router-outlet>
+            </app-layout>`
 })
-export class App implements OnInit {
-
-  constructor(private moviesMockService: MoviesMockService) {}
-
-  async ngOnInit() {
-    await lastValueFrom(this.moviesMockService.readCsvFile());
-    this.movies();
-  }
-
-  movies() {
-    // this.moviesMockService.getMovies({
-    //   page: 1,
-    //   size: 10,
-    //   year: 1980,
-    // })
-    // .subscribe(movies => {
-    //   console.log(movies);
-    // });
-    
-    // this.moviesMockService.getYearsWithMultipleWinners({
-    //   projection: 'years-with-multiple-winners',
-    // })
-    // .subscribe(movies => {
-    //   console.log(movies);
-    // });
-
-    // this.moviesMockService.getStudios({
-    //   projection: 'studios-with-win-count',
-    // })
-    // .subscribe(movies => {
-    //   console.log(movies);
-    // });
-
-    // this.moviesMockService.getIntervalWin({
-    //   projection: 'max-min-win-interval-for-producer',
-    // })
-    // .subscribe(movies => {
-    //   console.log(movies);
-    // });
-
-    this.moviesMockService.getMovieByYear({
-      winner: true,
-      year: 1980,
-    })
-    .subscribe(movies => {
-      console.log(movies);
-    });
-  }
-}
+export class App { }
