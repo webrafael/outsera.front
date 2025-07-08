26ee5faf606329cdf9a80e8d2af3d8ead8c10c48
Rafael Andrade <rafael.io@outlook.com>
2025-07-07 21:38:16 -0300

    Melhorias e correções de paginação

--- src/app/features/movies/movies.component.ts
+++ src/app/features/movies/movies.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { MoviePageResponse, MovieQueryParams } from '@shared/models/movie.model';
 
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { MovieService } from '@shared/services/movies/movie.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movies',
@@ -12,6 +14,7 @@ import { MovieService } from '@shared/services/movies/movie.service';
 })
 export class MoviesComponent implements OnInit {
 
+  private destroyRef = inject(DestroyRef);
   private movieService = inject(MovieService);
 
   public pages: number[] = [];
@@ -22,13 +25,41 @@ export class MoviesComponent implements OnInit {
   public currentYear = new Date().getFullYear();
   public filterYear?: number;
   public filterWinner: boolean = true;
+  public loading: boolean = false;
 
   ngOnInit(): void {
     this.makeMoviesTable();
   }
 
-  makeMoviesTable() {
+  // TODO: implementar teste unitário para esta função (onChangeWinner)
+  onChangeWinner(event: Event) {
+    const input = event.target as HTMLInputElement;
 
+    if (input.value !== this.filterWinner?.toString()) {
+      this.resetPagination();
+      return;
+    }
+
+    this.filterWinner = input.value === 'true';
+  }
+
+  // TODO: implementar teste unitário para esta função (onChangeYear)
+  onChangeYear(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.value !== this.filterYear?.toString()) {
+      this.resetPagination();
+      return;
+    }
+
+    this.filterYear = Number(input.value);
+  }
+
+  // TODO: refatorar teste unitário para esta função (makeMoviesTable)
+  makeMoviesTable() {
+    this.loading = true;
+
+    // Sempre que fizer uma nova pesquisa, a paginação deve ser resetada
     const params: Partial<MovieQueryParams> = {
       page: 0,
       size: 10
@@ -54,8 +85,13 @@ export class MoviesComponent implements OnInit {
     if (this.pageSize) {
       params.size = this.pageSize;
     }
+
     this.movieService
         .getMovies(params)
+        .pipe(
+          takeUntilDestroyed(this.destroyRef),
+          finalize(() => this.loading = false) // para evitar memory leaks
+        )
         .subscribe({
           next: (movies) => {
             this.movies = movies;
@@ -72,19 +108,38 @@ export class MoviesComponent implements OnInit {
         });
   }
 
+  // TODO: Implementar ou refatorar teste unitário para esta função (validateInput)
   validateInput(event: Event) {
-    // validar para digitar no máximo 4 caracteres
+
     const input = event.target as HTMLInputElement;
+
+    // deve aceitar somente números
+    if (isNaN(Number(input.value))) {
+      input.value = '';
+      return;
+    }
+
+    // deve aceitar no máximo 4 caracteres
     if (input.value.length > 4) {
       input.value = input.value.slice(0, 4);
     }
+
+    // Se o ano for maior que o ano atual, deve ser o ano atual
     if (Number(input.value) > this.currentYear) {
       input.value = this.currentYear.toString();
     }
   }
 
+  // TODO: Implementar ou refatorar teste unitário para esta função (goToPage)
   goToPage(page: number) {
     this.currentPage = page;
+    this.makeMoviesTable();
+  }
+
+  // TODO: Implementar ou refatorar teste unitário para esta função (resetPagination)
+  private resetPagination() {
+    this.pageSize = 10;
+    this.currentPage = 0;
     this.makeMoviesTable();
   }
 }
